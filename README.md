9ac3a986111f073eaeea24b873a9cd4b987c35b9
Aevyrie <aevyrie@gmail.com>
2021-04-09 00:07:47 -0700

    bevy 0.5

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![CI](https://github.com/aevyrie/bevy_mod_bounding/workflows/CI/badge.svg?branch=master)](https://github.com/aevyrie/bevy_mod_bounding/actions?query=workflow%3A%22CI%22+branch%3Amaster)
 [![crates.io](https://img.shields.io/crates/v/bevy_mod_bounding)](https://crates.io/crates/bevy_mod_bounding)
-[![docs.rs](https://docs.rs/bevy_mod_picking/badge.svg)](https://docs.rs/bevy_mod_bounding)
+[![docs.rs](https://docs.rs/bevy_mod_bounding/badge.svg)](https://docs.rs/bevy_mod_bounding)
 [![Bevy tracking](https://img.shields.io/badge/Bevy%20tracking-main-lightblue)](https://github.com/bevyengine/bevy/blob/main/docs/plugins_guidelines.md#main-branch-tracking)
 
 Unofficial plugin for generating bounding volumes.
@@ -16,12 +16,11 @@ Unofficial plugin for generating bounding volumes.
 | ✅ | Bounding Sphere            |
 | ✅ | Axis Aligned Bounding Box  |
 | ✅ | Oriented Bounding Box      |
-| ❌ | Convex Hull                |
 
-## Demo
+## Example
 
 Run the demo with:
 
 ```shell
-cargo run --example demo --features="ex"
+cargo run --features ex --example demo
 ```
--- examples/demo.rs
+++ examples/demo.rs
@@ -16,22 +16,20 @@ struct Rotator;
 
 fn setup(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let mesh_path = "models/waterbottle/WaterBottle.gltf#Mesh0/Primitive0";
-    let _scenes: Vec<HandleUntyped> = asset_server.load_folder("models").unwrap();
     let mut ortho_cam = OrthographicCameraBundle::new_3d();
     ortho_cam.transform = Transform::from_matrix(Mat4::face_toward(
-        Vec3::new(-0.5, 0.5, 3.0),
-        Vec3::new(0.0, 0.0, 0.0),
-        Vec3::new(0.0, 1.0, 0.0),
+        Vec3::new(1.0, 1.0, 5.0),
+        Vec3::ZERO,
+        Vec3::Y,
     ));
     commands.spawn_bundle(ortho_cam);
     // AABB
     commands
         .spawn_bundle(PbrBundle {
-            mesh: asset_server.get_handle(mesh_path),
+            mesh: meshes.add(Mesh::from(shape::Cube::default())),
             material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
             transform: Transform::from_translation(Vec3::new(-1.0, 0.0, 0.0)),
             ..Default::default()
@@ -42,7 +40,7 @@ fn setup(
     // OBB
     commands
         .spawn_bundle(PbrBundle {
-            mesh: asset_server.get_handle(mesh_path),
+            mesh: meshes.add(Mesh::from(shape::Cube::default())),
             material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
             transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
             ..Default::default()
@@ -53,7 +51,7 @@ fn setup(
     // Sphere
     commands
         .spawn_bundle(PbrBundle {
-            mesh: asset_server.get_handle(mesh_path),
+            mesh: meshes.add(Mesh::from(shape::Cube::default())),
             material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
             transform: Transform::from_translation(Vec3::new(1.0, 0.0, 0.0)),
             ..Default::default()
@@ -71,7 +69,7 @@ fn setup(
 /// Rotate the meshes to demonstrate how the bounding volumes update
 fn rotation_system(time: Res<Time>, mut query: Query<&mut Transform, With<Rotator>>) {
     for mut transform in query.iter_mut() {
-        let scale = Vec3::ONE * ((time.seconds_since_startup() as f32).sin() + 2.0);
+        let scale = Vec3::ONE * ((time.seconds_since_startup() as f32).sin() * 0.3 + 1.0) * 0.3;
         let rot_x = Quat::from_rotation_x((time.seconds_since_startup() as f32 / 5.0).sin() / 50.0);
         let rot_y = Quat::from_rotation_y((time.seconds_since_startup() as f32 / 3.0).sin() / 50.0);
         let rot_z = Quat::from_rotation_z((time.seconds_since_startup() as f32 / 4.0).sin() / 50.0);
